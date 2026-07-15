a260b171ff3b92e3286329bf5edb991d3622f13e
codes-sami <codes.sami@gmail.com>
2026-07-15 23:58:02 +0600

    Shorthand operator task done

---
+++ shorthand.js
@@ -0,0 +1,16 @@
+let score = 50;
+
+score += 10;
+console.log("After +=: ",score);
+
+score -= 5;
+console.log('After -=: ', score);
+
+score *= 2;
+console.log('After *=2: ', score);
+
+score /= 5;
+console.log("After /=: ", score);
+
+score %= 4;
+console.log("After %=: ", score);
